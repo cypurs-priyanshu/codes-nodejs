ca15f4d3a82f0cbb2a2eb2a7fa3a33d7b3e27626
cypurs_priyanshu <priyanshu.cypurs@gmail.com>
2019-03-14 20:19:59 +0530

    14 mar 2019 object array

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.use(express.json());
 app.get('/user', function (req, res) {
     console.log("You requested for a user");
     res.json(dataSet)
-     res.send(dataSet);
+    res.send(dataSet);
     // res.sendStatus(200);
 })
 app.get('/count', function (req, res) {
-let count = dataSet.length
-console.log(count);
-res.send(count+"");
-// res.end(count)
+    let count = dataSet.length
+    console.log(count);
+    res.send(count + "");
+    // res.end(count)
 }
 )
 app.get('/user/:id', function (req, res) {
@@ -30,7 +30,7 @@ app.get('/user/:id', function (req, res) {
         } else {
             res.sendStatus(404);
             // res.send("No Data Available");
-            
+
         }
     }
     catch (err) {
@@ -47,18 +47,66 @@ app.get('/user/:id', function (req, res) {
 app.listen(PORT, function () {
     console.log("Server running...")
 })
-app.post('/user', function (req, res) {
+/*app.post('/user', function (req, res) {
     let data = req.body
-    
-    if(data != undefined && data != null){
+
+    if (data != undefined && data != null) {
         dataSet.push(data)
         console.log(dataSet);
         res.json(dataSet)
     }
-    else{
+    else {
         res.sendStatus(500)
     }
-    
-
 }
-)
+)*/
+app.post('/user', function (req, res) {
+    let info = req.body;
+    console.log(info);
+    if (info != undefined && info != null) {
+        if (Array.isArray(info)) // if the requested data is array.
+        {
+            console.log("block array")
+            for (var i = 0; i < info.length; i++) {
+                console.log("block loop")
+                var v = info[i];
+                dataSet.push(v)
+
+                console.log("you are in the block")
+
+
+            }
+            console.log(dataSet);
+            res.json(dataSet)
+            // send.req(dataSet);
+        }
+       else  if ( typeof info == "object") {
+            console.log("out")
+            var v;
+                //when data is object 
+            {   
+                console.log("data is object")
+                // var v = info(req.body);
+                dataSet.push(info);
+                console.log(dataSet);
+                res.json(dataSet);      //if the requested data is object.
+            }
+            
+          /*  if (   toString(info))         // when data is string or other
+
+            {
+
+                console.log("Error in your requested data")
+            }
+                else
+                {
+                    return "not"
+
+                }
+                */
+        }
+        else{
+            res.send("INVALID INPUT DATA");
+        }
+    }
+    })
